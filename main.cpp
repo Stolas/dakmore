dbcba3ef7136d910d4b8921e18a7d923850f9f02
Robin Vossen <robin@codeinject.org>
2016-04-10 08:55:43 +0200

    Demo builds

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "libtcod.hpp"
+
+int main(int argc, char **argv)
+{
+	TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
+	while(!TCODConsole::isWindowClosed()) {
+		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, NULL, NULL);
+		TCODConsole::root->clear();
+		TCODConsole::root->putChar(40, 25, '@');
+		TCODConsole::flush();
+	}
+	return 0;
+}
+
